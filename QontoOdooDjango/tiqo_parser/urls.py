4ff8df190b2b93b65a0e363c08e3c42aa6556ba3
Jonas Legion <jturbeaux@pm.me>
2023-04-03 10:08:39 +0200

    url

--- QontoOdooDjango/tiqo_parser/urls.py
+++ QontoOdooDjango/tiqo_parser/urls.py
@@ -13,6 +13,6 @@ urlpatterns = [
     path('action/refresh_analytic_account', refresh_analytic_account.as_view(), name='refresh_analytic_account'),
     path('action/refresh_journal_account', refresh_journal_account.as_view(), name='refresh_journal_account'),
     path('action/refresh_account_account', refresh_account_account.as_view(), name='refresh_account_account'),
-    path('admin/', admin.site.urls),
-    path('', index.as_view(), name='index'),
+    # path('admin/', admin.site.urls),
+    # path('', admin.site.urls, name='index'),
 ]
