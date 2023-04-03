bf13b29d8e943079acda956a238f4afb53fcb53e
Jonas Legion <jturbeaux@pm.me>
2023-04-03 10:00:10 +0200

    debug false

--- QontoOdooDjango/QontoOdooDjango/settings.py
+++ QontoOdooDjango/QontoOdooDjango/settings.py
@@ -23,7 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-uz2!bw2nfg8o7^05sr4)(r90xwto5qtb%#=q*n8_g&rv(^)bew'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['https://tiqo.codecommun.re','tiqo.codecommun.re','localhost']
 
